8f53850371586248a722c47286397f47aea4dbb3
ashish19977 <ashishmoria96@gmail.com>
2020-05-15 23:16:43 +0530

    server changes

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const path = require('path')
 const app = express()
 
 app.use(express.static(path.join(__dirname, './public')))
+console.log(path.join(__dirname, './public'))
 const port=process.env.PORT||5050
 app.listen(port, () => {
     console.log(`Server started on ${port}`);
